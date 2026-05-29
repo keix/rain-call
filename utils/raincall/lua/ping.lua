fa0d4276d14650b507ddef7ce5333ce757929e75
Kei Sawamura <kei@9th.tokyo>
2026-05-30 04:41:44 +0900

    Add Rain-Call TCP backend roadmap

--- utils/raincall/lua/ping.lua
+++ utils/raincall/lua/ping.lua
@@ -10,3 +10,8 @@ local pong = RAIN.CALL("PING")
 assert(pong == "PONG")
 
 print("RAIN.CALL('PING') = " .. pong)
+
+assert(RAIN.CALL("SET", "moon", "quake") == "OK")
+assert(RAIN.CALL("GET", "moon") == "quake")
+
+print("RAIN.CALL('GET', 'moon') = " .. RAIN.CALL("GET", "moon"))
--- utils/raincall/raincall.h
+++ utils/raincall/raincall.h
@@ -9,7 +9,12 @@ extern "C" {
 
 #define RAINCALL_VERSION "0.1.0"
 
+typedef struct raincall_State raincall_State;
+
+raincall_State *raincall_open(const char *host, int port);
+int raincall_install_state(mq_State *L, raincall_State *R);
 int raincall_install(mq_State *L);
+void raincall_close(raincall_State *R);
 
 #ifdef __cplusplus
 }
